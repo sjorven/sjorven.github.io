a066bb7330cdb0a38bd5991cef1e5344ea80419e
Sjorven <95484407+sjorven@users.noreply.github.com>
2022-04-11 16:01:54 +0200

    tree change

--- fractal_tree/fractal.js
+++ fractal_tree/fractal.js
@@ -1,25 +1,26 @@
-var angle = 0;
+var angle = 1;
 var lengthShrink = 0.85;
 var widthShrink = 0.65;
 var x = 800;
 var y = 400;
-var branchLength = 0;
+var branchLength = 5;
 var logWidth = 0;
 
 function setup(){
   createCanvas(800, 400);
+  frameRate(20);
 }
 
 function draw(){
   background(135, 206, 235);
   if (angle >= PI/4 || branchLength >= y/6 || logWidth >= y/8){
-      angle = PI/15;
-      branchLength = 10;
+      angle = random(PI/13,PI/15);
+      branchLength = random(10);
       logWidth = 0.01;
     } else {  
       branchLength += 0.3;
       logWidth += 0.1;
-      angle += PI/3000;
+      angle += PI/2000;
       stroke(255,255,255);    
   }
   translate(x/2, y);
