6d6cce345d6734c92e1781696f4c43c2ba6246d9
Sjorven <95484407+sjorven@users.noreply.github.com>
2022-06-04 08:42:15 +0200

    changes

--- illusion/index.js
+++ illusion/index.js
@@ -5,24 +5,27 @@ const sinusFactor = 0.8
 const horizontalBars = 15
 const strokeWidth = 1
 
+const canvas = d3.select("#canvas")
+
+//array for x offset of the 8 rectangles
 var rect = [0, 0, 0, 0, 0, 0, 0, 0]
 
+//setting slider value to initial zero,
+//adding listener to the sliders
+d3.select("#slider").node().value = 0
 d3.select("#slider").on("input", () => {
     update(d3.select("#slider").node().value)
 })
 
-function update(x) {
-    rect.forEach((element, i) => {
-        rect[i] = Math.sin(i * sinusFactor)*x
-    })
-    console.log(x)
-    console.log(rect)
-    canvas.selectAll("g").remove()
+//applying x sinus offset according to slider value
+function update(factor) {
+    for(i = 0; i<rect.length;i++){
+        rect[i] = Math.sin(i * sinusFactor) * factor
+    }
     updateVis()
 }
 
-const canvas = d3.select("#canvas")
-
+//creating the horizontal lines
 for (var i = 1; i < rect.length * 3; i++) {
     canvas.append("line")
         .attr("stroke", "black")
@@ -32,39 +35,25 @@ for (var i = 1; i < rect.length * 3; i++) {
         .attr("y2", height / (rect.length * 3) * i)
 }
 
+//updating the rectangles x positions
 function updateVis() {
+    canvas.selectAll("g").remove()
     var rectGroups = canvas.selectAll("g")
         .data(rect)
-        .enter().append("g")
+        
+    var enteredGroups = rectGroups.enter().append("g")
 
     for (var j = 0; j <= horizontalBars; j++) {
-        rectGroups.append("rect")
-            .attr("x", d => {
-                return d + width / horizontalBars * j
-            })
-            .attr("y", (d, i) => height / 3 / rect.length * i)
-            .attr("width", width / horizontalBars / 2)
-            .attr("height", height / 3 / rect.length)
-            .attr("fill", "black")
-
-        rectGroups.append("rect")
-            .attr("x", d => {
-                return d + width / horizontalBars * j
-            })
-            .attr("y", (d, i) => height / 3 / rect.length * i + height / 3)
-            .attr("width", width / horizontalBars / 2)
-            .attr("height", height / 3 / rect.length)
-            .attr("fill", "black")
-
-        rectGroups.append("rect")
-            .attr("x", d => {
-                return d + width / horizontalBars * j
-            })
-            .attr("y", (d, i) => height / 3 / rect.length * i + height / 3 * 2)
-            .attr("width", width / horizontalBars / 2)
-            .attr("height", height / 3 / rect.length)
-            .attr("fill", "black")
+        for (var k = 0; k < 3; k++) {
+            enteredGroups.append("rect")
+                .attr("x", d => {
+                    return d + width / horizontalBars * j
+                })
+                .attr("y", (d, i) => height / 3 / rect.length * i + (height/3)*k)
+                .attr("width", width / horizontalBars / 2)
+                .attr("height", height / 3 / rect.length)
+                .attr("fill", "black")
+        }
     }
 }
 updateVis()
-
